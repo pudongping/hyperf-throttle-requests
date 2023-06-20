a9ffdf2e9f0df1f6134cfc98d1499a8a247607da
pudongping <276558492@qq.com>
2023-06-20 14:09:19 +0800

    delete default value

--- src/Annotation/ThrottleRequests.php
+++ src/Annotation/ThrottleRequests.php
@@ -22,31 +22,31 @@ class ThrottleRequests extends AbstractAnnotation
     /**
      * @var int
      */
-    public $maxAttempts = 60;
+    public $maxAttempts;
 
     /**
      * @var int
      */
-    public $decaySeconds = 60;
+    public $decaySeconds;
 
     /**
      * @var string
      */
-    public $prefix = '';
+    public $prefix;
 
     /**
      * @var string
      */
-    public $key = '';
+    public $key;
 
     /**
      * @var null|callable
      */
-    public $generateKeyCallable = [];
+    public $generateKeyCallable;
 
     /**
      * @var null|callable
      */
-    public $tooManyAttemptsCallback = [];
+    public $tooManyAttemptsCallback;
 
 }
